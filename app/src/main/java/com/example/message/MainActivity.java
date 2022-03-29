e7e33887dad60f2d23866252fb8f8fe1e35934c0
Никита <pedichn@gmail.com>
2022-03-29 19:03:06 +0300

    Готово

--- app/src/main/java/com/example/message/MainActivity.java
+++ app/src/main/java/com/example/message/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,21 +22,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dialog = new Dialog(MainActivity.this);
-
-        // Установите заголовок
-        dialog.setTitle("Заголовок диалога");
-        // Передайте ссылку на разметку
-        dialog.setContentView(R.layout.dialog_view);
-        // Найдите элемент TextView внутри вашей разметки
-        // и установите ему соответствующий текст
-        TextView text = (TextView) dialog.findViewById(R.id.textView);
-        text.setText("Текст в диалоговом окне. Вы любите котов?");
     }
 
     public void onClick(View v)
     {
-        // Выводим диалоговое окно на экран
-        dialog.show();
+        CheckBox apple = findViewById(R.id.checkBoxApples);
+        CheckBox strawberry = findViewById(R.id.checkBoxStrawberry);
+        CheckBox blueberry = findViewById(R.id.checkBoxBlueBerry);
+        CheckBox potatoes = findViewById(R.id.checkBoxPotatoes);
+
+        EditText textApples = findViewById(R.id.editTextApples);
+        EditText textblueberry = findViewById(R.id.editTextBlueBerry);
+        EditText textStrawberry = findViewById(R.id.editTextStrawberry);
+        EditText textPotatoes = findViewById(R.id.editTextPotatoes);
+
+        String cart = "";
+        int price = 0;
+        int templeCount;
+        if (apple.isChecked())
+        {
+            templeCount = Integer.parseInt(String.valueOf(textApples.getText()));
+            price += templeCount * 200;
+            cart += "Яблоки\t\t\t\t" + templeCount + "кг\t\t" + "всего: " + (templeCount * 200) +"₽\n";
+        }
+        if (strawberry.isChecked())
+        {
+            templeCount = Integer.parseInt(String.valueOf(textStrawberry.getText()));
+            price += templeCount * 800;
+            cart += "Клубника\t\t" + templeCount + "кг\t\t" + "всего: " + (templeCount * 800) +"₽\n";
+        }
+        if (blueberry.isChecked())
+        {
+            templeCount = Integer.parseInt(String.valueOf(textblueberry.getText()));
+            price += templeCount * 700;
+            cart += "Голубика\t\t" + templeCount + "кг\t\t" + "всего: " + (templeCount * 700) +"₽\n";
+        }
+        if (potatoes.isChecked())
+        {
+            templeCount = Integer.parseInt(String.valueOf(textPotatoes.getText()));
+            price += templeCount * 100;
+            cart += "Картошка\t" + templeCount + "кг\t\t" + "всего: " + (templeCount * 100) +"₽\n";
+        }
+
+
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+        a_builder.setMessage(cart + "\nИтого:"+price + '₽').setTitle("Ваш заказ").setIcon(R.drawable.shop);
+        a_builder.show();
     }
 }
